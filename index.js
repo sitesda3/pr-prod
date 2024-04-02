dc06284ad3d812e7b373e75211921c662e8ef143
sitesda3 <86269726+sitesda3@users.noreply.github.com>
2024-04-02 23:34:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,63 @@ const redirectURL = (res, url) => {
 		res.end();
 }
 
+
+
+
+  console.log("START PUPPETEER")
+	
+  let URL = "https://github.com/";
+  const browser = await puppeteer.launch({
+
+	args: [
+		"--disable-setuid-sandbox",
+		"--no-sandbox",
+		"--single-process",
+		"--no-zygote",
+	  ],
+	
+	headless: 'new',
+
+	executablePath: process.env.NODE_ENV === "production"
+	? process.env.PUPPETEER_EXECUTABLE_PATH
+	: puppeteer.executablePath()
+  });
+
+  const page = await browser.newPage();
+  
+try {
+  
+  const customUA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36';
+ 
+  await page.setUserAgent(customUA);
+  
+  await page.setViewport({width: 1920, height: 1080});
+  
+  await page.setRequestInterception(true);
+  
+  page.on('request', interceptedRequest => {
+    if ( interceptedRequest.isInterceptResolutionHandled() ) return;
+    if ( interceptedRequest.url().includes(KEYWORD) ) {
+	   URL = interceptedRequest.url();
+	} else {
+		interceptedRequest.continue();
+	}
+  });
+  
+  await page.goto(tURL);
+
+  
+
+		
+ } catch (e) {
+	console.log(e);
+ } finally {
+	await browser.close();
+ }
+
+
+
+
 const getURL = async (req, res, tURL, route) => {
 	
   console.log(tURL)
